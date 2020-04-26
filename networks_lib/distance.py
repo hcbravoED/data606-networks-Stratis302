8a17a51f5c93b49c0bb21da7bc7002d168e331e0
Stratis302 <stratis.aloimonos@gmail.com>
2020-04-26 00:38:41 -0400

    My commit message

--- networks_lib/distance.py
+++ networks_lib/distance.py
@@ -14,11 +14,29 @@ from collections import deque
 def bfs_distance(mat):
     num_vertices = mat.shape[0]    
     res = np.full((num_vertices, num_vertices), np.inf)
-    visited = np.full((num_vertices), False)
     
     # Finish this loop
     for i in range(num_vertices):
-        pass
+        visited = np.full((num_vertices), False)
+        Q = deque()
+        Q.append([i, 0])
+        
+        while len(Q) > 0:
+            x = Q.popleft()
+            #print(x)
+            visited[x[0]] = True
+            #print(x[0])
+            #print(visited)
+            res[i, x[0]] = x[1]
+            #print(res)
+            
+            neighbors = np.where(mat[x[0],:]>0)[0]    
+            #print(neighbors)
+            for w in neighbors:
+                if visited[w] == False:
+                    Q.append([w, x[1] + 1])
+                    visited[w] = True
+            #print(Q)
     return res
 
 ## TODO: Implement this function
@@ -34,15 +52,22 @@ def bfs_distance(mat):
 def get_components(mat):
     dist_mat = bfs_distance(mat)
     num_vertices = mat.shape[0]
-    available = [False for _ in range(num_vertices)]
+    available = [True for _ in range(num_vertices)]
 
     components = []
-    
     # finish this loop
     while any(available):
-        pass
-    
-    # this is for testing purposes remove from final solution
-    components = [np.arange(num_vertices)]
+        x = []
+        u = np.argmax(available) #Possibly rearrange later
+       #print(u)
+        for i in range(num_vertices):
+            if dist_mat[u, i] < np.inf:
+                x.append(i)
+            available[u] = False
+        if x not in components:
+            components.append(x)
+        #print(components)
+        
+
     
     return components
